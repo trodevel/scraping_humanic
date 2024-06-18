9f8090826a1cc2a6db7f43b380d3643db8637944
trodevel <trodevel@gmail.com>
2024-06-18 20:16:13 +0200

    intermediate results

--- test/test_01.py
+++ test/test_01.py
@@ -22,6 +22,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from scraping_helpers import helpers  # does_xpath_exist_with_timeout
 from print_helpers.helpers import print_fatal, print_debug, set_log_level, DEBUG
+from scraping_humanic.scraping_humanic import round_around_xpath
 
 import config         # DRIVER_PATH
 from test_01_xpath import URL, BANNER, PATH
@@ -51,7 +52,9 @@ def test_01():
         print_fatal( "broken xpath" )
         exit()
 
-    print_debug( "found xpath" )
+    print_debug( "found xpath, rotating" )
+
+    round_around_xpath( driver, PATH, 100 )
 
     helpers.sleep( wait_min * 60 )
 
