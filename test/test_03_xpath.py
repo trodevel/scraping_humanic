4c461af6ca370e7f1c715adea08774d170a5e1e0
trodevel <trodevel@gmail.com>
2024-06-19 01:30:43 +0200

    added test_03.py

---
+++ test/test_03_xpath.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+URL = 'https://www.google.com'
+
+BUTTONS     = ( '/html/body'
+                '/div'
+                '/div[@role="dialog"]'
+                '/div'
+                '/span'
+                '/div'
+                '/div'
+                '/div'
+                '/div'
+                '/div'
+                '/button'
+                )
